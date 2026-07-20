41ddce1579dbc279bbe0b7532a422174ca13bdc0
Ashwin Maurya <ashwinmaurya3691@gmail.com>
2026-07-21 01:34:00 +0530

    feat: implement user registration with MongoDB Atlas

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,17 +1,46 @@
 const validator = require("validator");
+const bcrypt = require("bcrypt");
 const User = require("../models/user.model")
 
 const register = async(req, res)=>{
-  const {name, email, password} = req.body;
+  const { name, email, password } = req.body;
   if( !name || !email || !password){
     return res.status(400).json({message:"Name, email, and password are required."});
   }
   if(!validator.isEmail(email)){
     return res.status(400).json({message:"Please provide a valid email address."});
   }
-  const existingUser = await User.findOne({ email });
-  if(existingUser){
-    return res.status(400).json({message: "User already exists"});
+ 
+  
+  if (!validator.isStrongPassword(password, {
+      minLength: 8,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 0
+      })
+    ) {
+      return res.status(400).json({
+          message: "Password does not meet security requirements."
+      });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if(existingUser){
+      return res.status(400).json({message: "User already exists"});
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword
+    });
+    await newUser.save();
+    return res.status(201).json({ message: "Registered Successfully"});
+  } 
+  catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Something went wrong."});
   }
 };
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
 
 const app = require("./app");
-const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000
+
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startServer();
+
