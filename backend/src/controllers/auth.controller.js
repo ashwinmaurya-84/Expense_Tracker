316b122fee6473c0da26a05635d37658a43894e0
Ashwin Maurya <ashwinmaurya3691@gmail.com>
2026-07-21 23:53:27 +0530

    feat: implement user login authentication

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -44,4 +44,28 @@ const register = async(req, res)=>{
   }
 };
 
-module.exports = {register};
+const login = async(req, res)=>{
+  const { email, password } = req.body;
+  if( !email || !password){
+    return res.status(400).json({message:"Email and password are required."});
+  }
+  if(!validator.isEmail(email)){
+    return res.status(400).json({message:"Please provide a valid email address."});
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if ( !user ){
+      return res.status(401).json({ message: "Invalid email or password"});
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid){
+      return res.status(401).json({ message: "Invalid email or password"});
+    }
+    return res.status(200).json({ message: "Login Successful"});
+  }catch (err){
+    return res.status(500).json({ message: "Something went wrong"});
+  }
+};
+
+module.exports = {register, login};
